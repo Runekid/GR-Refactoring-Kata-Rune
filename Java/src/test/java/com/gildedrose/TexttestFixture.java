1dc4b3abc09a3d490f1fb4b76f930bafa46f4a3f
runekid <bugdayci.onur@aol.com>
2026-05-28 01:19:12 +0200

    updated java, junit and maven versions and edited the tests

--- Java/src/test/java/com/gildedrose/TexttestFixture.java
+++ Java/src/test/java/com/gildedrose/TexttestFixture.java
@@ -1,7 +1,7 @@
 package com.gildedrose;
 
 public class TexttestFixture {
-    public static void main(String[] args) {
+    void main(String[] args) {
         System.out.println("OMGHAI!");
 
         Item[] items = new Item[] {
