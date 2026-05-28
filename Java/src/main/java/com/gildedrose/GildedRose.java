91116688c63212b32ad932828742ada4d4589610
runekid <bugdayci.onur@aol.com>
2026-05-28 03:09:24 +0200

    handled ide warnings about some of the isSulfuras conditions

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -18,6 +18,10 @@ class GildedRose {
         boolean isBackstagePass = item.name.equals("Backstage passes to a TAFKAL80ETC concert");
         boolean isSulfuras = item.name.equals("Sulfuras, Hand of Ragnaros");
 
+        if (isSulfuras) {
+            return;
+        }
+
         if (isAgedBrie) {
             if (item.quality < 50) {
                 item.quality = item.quality + 1;
@@ -55,28 +59,16 @@ class GildedRose {
             }
 
         } else {
-            if (isSulfuras) {
-                if (item.quality > 0) {
-                }
+            if (item.quality > 0) {
+                item.quality = item.quality - 1;
+            }
 
-                if (item.sellIn < 0) {
-                    if (item.quality > 0) {
-                    }
-                }
+            item.sellIn = item.sellIn - 1;
 
-            } else {
+            if (item.sellIn < 0) {
                 if (item.quality > 0) {
                     item.quality = item.quality - 1;
                 }
-
-                item.sellIn = item.sellIn - 1;
-
-                if (item.sellIn < 0) {
-                    if (item.quality > 0) {
-                        item.quality = item.quality - 1;
-                    }
-                }
-
             }
         }
     }
