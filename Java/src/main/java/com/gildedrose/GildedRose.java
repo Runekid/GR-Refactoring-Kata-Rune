139e7690483fdf5b59808216d26b7669ef10db69
runekid <bugdayci.onur@aol.com>
2026-05-28 02:32:12 +0200

    lifted aged brie condition and extracted it to separate method after which I inlined the extracted method for simplifying logic

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -14,14 +14,8 @@ class GildedRose {
     }
 
     private void updateItem(Item item) {
-        if (!item.name.equals("Aged Brie")
-            && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (item.quality > 0) {
-                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                    item.quality = item.quality - 1;
-                }
-            }
-        } else {
+        boolean isAgedBrie = item.name.equals("Aged Brie");
+        if (isAgedBrie) {
             if (item.quality < 50) {
                 item.quality = item.quality + 1;
 
@@ -39,14 +33,48 @@ class GildedRose {
                     }
                 }
             }
-        }
 
-        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-            item.sellIn = item.sellIn - 1;
-        }
+            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                item.sellIn = item.sellIn - 1;
+            }
 
-        if (item.sellIn < 0) {
-            if (!item.name.equals("Aged Brie")) {
+            if (item.sellIn < 0) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+                }
+            }
+        } else {
+            if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (item.quality > 0) {
+                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                        item.quality = item.quality - 1;
+                    }
+                }
+            } else {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+
+                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                        if (item.sellIn < 11) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1;
+                            }
+                        }
+
+                        if (item.sellIn < 6) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1;
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                item.sellIn = item.sellIn - 1;
+            }
+
+            if (item.sellIn < 0) {
                 if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                     if (item.quality > 0) {
                         if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
@@ -56,11 +84,8 @@ class GildedRose {
                 } else {
                     item.quality = 0;
                 }
-            } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
             }
         }
     }
+
 }
